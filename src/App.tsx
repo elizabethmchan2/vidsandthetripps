7decba9401e20999726e8223454ecdb62b7a3968
Elizabeth Chan <elizabethchan@Elizabeths-MacBook-Air.local>
2024-12-18 21:22:02 -0800

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import Loader from './Loader/Loader';
+import './App.scss'
+
+function App() {
+  return (
+    <div className='main'>
+      <Loader />
+    </div>
+  )
+}
+
+export default App
---
+++ src/Loader/Loader.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import styles from './loading.module.scss';
+
+const Loader = () => {
+  return (
+    <div className={styles.loading}>Loader</div>
+  )
+}
+
+export default Loader;
