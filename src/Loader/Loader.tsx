0994d5f93a7599f1a015de786aa96597761d5a69
elizabethmchan2 <elizabethmchan2@gmail.com>
2024-12-19 01:00:22 -0800

    unsure why building css modules isnt working

--- src/Loader/Loader.tsx
+++ src/Loader/Loader.tsx
@@ -2,16 +2,17 @@ import { Platter } from '../assets/Platter';
 import { Tonearm } from '../assets/Tonearm';
 import { Vinyl } from '../assets/Vinyl';
 import VinylContrast from '../assets/VinylContrast';
-import styles from './Loading.module.scss';
+import './Loading.scss';
 
+// TODO: fix css modules
 const Loader = () => {
   return (
-    <div className={styles.loading}>
-        <div className={styles.wrapper}>
-            <Vinyl className={styles.vinyl}/>
-            <Platter className={styles.platter}/>
-            <VinylContrast className={styles.vinylContrast} />
-            <Tonearm className={styles.tonearm}/>
+    <div className='loading'>
+        <div className='wrapper'>
+            <Vinyl className='vinyl'/>
+            <Platter className='platter'/>
+            <VinylContrast className='vinylContrast' />
+            <Tonearm className='tonearm'/>
         </div>
     </div>
   )
