3bfeee6f7532ff762bd102948a6d0c67d0f23d20
elizabethmchan2 <elizabethmchan2@gmail.com>
2025-08-04 22:53:37 -0700

    add nav on smaller screen

--- src/MainContainer/MainContainer.tsx
+++ src/MainContainer/MainContainer.tsx
@@ -1,27 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Face from '../assets/home/Face';
 import Floor from '../assets/home/Floor';
 import { useLocation } from 'react-router';
 import './MainContainer.scss';
 import classNames from 'classnames';
+import { useWindowSize } from '@uidotdev/usehooks';
+import { Hamburger } from '../assets/nav/Hamburger';
+import { Bookshelf } from '../assets/home/Bookshelf';
 
-const MainContainer = ({
-  children,
-  childrenContainerClassname,
+const NavBar = ({
+  aClassnamesWithNavLink,
 }: {
-  children: React.ReactNode;
-  childrenContainerClassname?: string;
+  aClassnamesWithNavLink: (val: string) => string;
 }) => {
-  const location = useLocation();
+  const size = useWindowSize();
+  const [showNavMobile, setShowNavMobile] = useState(false);
 
-  const aClassnamesWithNavLink = (navLink: string) => {
-    return classNames('nav-link', {
-      ['nav-link-active']: location.pathname === navLink,
-    });
-  };
-
-  return (
-    <div className="mainContainer">
+  if (size?.width && size.width >= 768) {
+    return (
       <nav className="navbar">
         <ul className="nav">
           <li className="nav-item">
@@ -56,6 +52,93 @@ const MainContainer = ({
           </li>
         </ul>
       </nav>
+    );
+  }
+
+  return (
+    <nav className="navbar">
+      <ul className="nav">
+        <li className="nav-item">
+          <a className="nav-link" href="/home">
+            <Face className="facePoster" />
+          </a>
+        </li>
+        <button className="hamburger" onClick={() => setShowNavMobile(true)}>
+          <Hamburger />
+        </button>
+        {showNavMobile && (
+          <div className="drawer-nav">
+            <div className="drawer-nav-header">
+              <Face className="facePoster" />
+              <button
+                className="drawer-nav-close"
+                onClick={() => setShowNavMobile(false)}
+              >
+                X
+              </button>
+            </div>
+            <ul className="drawer-nav-list">
+              <li className="nav-item">
+                <a className={aClassnamesWithNavLink('/home')} href="/home">
+                  Home
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className={aClassnamesWithNavLink('/bio')} href="/bio">
+                  Bio
+                </a>
+              </li>
+              <li className="nav-item">
+                <a
+                  className={aClassnamesWithNavLink('/writings')}
+                  href="/writings"
+                >
+                  Writings
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className={aClassnamesWithNavLink('/zines')} href="/zines">
+                  Zines
+                </a>
+              </li>
+              <li className="nav-item">
+                <a
+                  className={aClassnamesWithNavLink('/contact')}
+                  href="/contact"
+                >
+                  Contact
+                </a>
+              </li>
+            </ul>
+            <div className="drawer-footer">
+              <Bookshelf className="bookshelf" />
+              <Floor className="floor" />
+            </div>
+          </div>
+        )}
+      </ul>
+    </nav>
+  );
+};
+
+const MainContainer = ({
+  children,
+  childrenContainerClassname,
+}: {
+  children: React.ReactNode;
+  childrenContainerClassname?: string;
+}) => {
+  const location = useLocation();
+
+  const aClassnamesWithNavLink = (navLink: string) => {
+    return classNames('nav-link', {
+      ['nav-link-active']: location.pathname === navLink,
+    });
+  };
+
+  return (
+    <div className="mainContainer">
+      <NavBar aClassnamesWithNavLink={aClassnamesWithNavLink} />
       <div
         className={`mainContainerChildrenContainer ${childrenContainerClassname || ''}`}
       >
---
+++ src/assets/nav/Hamburger.tsx
@@ -0,0 +1,37 @@
+export const Hamburger = ({ className }: { className?: string }) => {
+  return (
+    <svg
+      className={className}
+      width="40"
+      height="35"
+      viewBox="0 0 40 35"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <rect
+        x="0.324951"
+        y="0.540039"
+        width="38.7363"
+        height="8.80078"
+        rx="2"
+        fill="#291F1F"
+      />
+      <rect
+        x="0.324951"
+        y="13.0879"
+        width="38.7363"
+        height="8.80078"
+        rx="2"
+        fill="#291F1F"
+      />
+      <rect
+        x="0.324951"
+        y="25.6357"
+        width="38.7363"
+        height="8.80078"
+        rx="2"
+        fill="#291F1F"
+      />
+    </svg>
+  );
+};
