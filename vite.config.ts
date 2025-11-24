067e8bde0a9c8976bcc362f373aae83ab3e89d79
elizabethmchan2 <elizabethmchan2@gmail.com>
2025-11-23 22:25:33 -0800

    add image preloader and clean up unused poster items

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,16 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import UnpluginInjectPreload from 'unplugin-inject-preload/vite';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    UnpluginInjectPreload({
+      files: [
+        { entryMatch: /[a-zA-Z]*\.jpg$/ },
+        { entryMatch: /[a-zA-Z]*\.webp$/ },
+      ],
+    }),
+  ],
 });
