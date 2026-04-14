c83d6c36f395b599491b401e172fcd634da18ef3
elizabethmchan2 <elizabethmchan2@gmail.com>
2026-04-13 20:36:48 -0700

    update bio

--- src/screens/Bio/Bio.tsx
+++ src/screens/Bio/Bio.tsx
@@ -8,12 +8,12 @@ const Bio = () => {
       <div className="bio">
         <div className="bioLeft">
           <p>
-            Vida Hasson is a writer, music critic, and zine artist based in
-            Seattle and San Francisco. Her writing has appeared in Razorcake,
-            Our Culture Mag, New Noise Magazine, The Line of Best Fit, and Under
-            the Radar. She graduated from Claremont McKenna College with an
-            honors degree in Literature and a minor in Gender and Sexuality
-            Studies, completing her{' '}
+            Vida Hasson is a writer, music critic, and zine artist from San
+            Francisco. Her writing has appeared in Razorcake, Our Culture Mag,
+            New Noise Magazine, The Line of Best Fit, and Under the Radar. She
+            graduated from Claremont McKenna College with an honors degree in
+            Literature and a minor in Gender and Sexuality Studies, completing
+            her{' '}
             <a
               href="https://scholarship.claremont.edu/cgi/viewcontent.cgi?params=/context/cmc_theses/article/4273/&path_info=Performing_Grrrlhood__A_Lyrical_Analysis_of_Riot_Grrrl_Music.pdf"
               target="_blank"
@@ -21,7 +21,7 @@ const Bio = () => {
             >
               undergraduate thesis
             </a>{' '}
-            on Riot Grrrl song lyrics. She loves feminist punk.
+            on Riot Grrrl song lyrics.
           </p>
         </div>
         <div className="bioRight">
